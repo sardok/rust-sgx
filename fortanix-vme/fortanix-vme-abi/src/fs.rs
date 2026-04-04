0c1c3298955a6b9eeca7e1b1cb20a18df2dd482d
Sinan Nalkaya <sardok@gmail.com>
2026-04-04 21:46:47 +0200

    Update fs abi & add new invariant implementations.

--- fortanix-vme/fortanix-vme-abi/src/fs.rs
+++ fortanix-vme/fortanix-vme-abi/src/fs.rs
@@ -1,28 +1,56 @@
-use std::vec::Vec;
 use std::fs::Metadata;
 use std::os::unix::fs::MetadataExt;
+use std::string::String;
+use std::vec::Vec;
 use serde::{Deserialize, Serialize};
 
 
 #[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub enum FsOpRequest {
-    Create,
+    Create {
+        // inode of parent
+        parent: u64,
+        name: String,
+        metadata: Vec<u8>,
+    },
     GetAttr {
         ino: u64,
     },
-    Mkdir,
-    Read,
+    Lookup {
+        ino: u64,
+        name: String,
+    },
+    Mkdir {
+        // inode of parent
+        ino: u64,
+        name: String,
+        metadata: Vec<u8>,
+    },
+    Read {
+        ino: u64,
+    },
     ReadDir {
         ino: u64,
+    },
+    RmDir {
+        ino: u64,
+        name: String,
     },
     SetAttr {
         ino: u64,
         metadata: Vec<u8>,
     },
-    Write,
+    Unlink {
+        ino: u64,
+        name: String,
+    },
+    Write {
+        ino: u64,
+        content: Vec<u8>,
+    },
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub enum FileType {
     RegularFile,
     Directory,
@@ -42,18 +70,30 @@ impl From<Metadata> for HostMetadata {
         };
 
         HostMetadata {
+            blocks: metadata.blocks(),
             ino: metadata.ino(),
             kind,
+            nlink: metadata.nlink() as u32,
+            rdev: metadata.rdev() as u32,
+            atime: metadata.atime() as u64,
+            mtime: metadata.mtime() as u64,
+            ctime: metadata.ctime() as u64,
         }
     }
 }
 
 /// This struct contains non-encrypted metadata about a file/directory entry
 /// to be used by enclave fuse implementation as is.
-#[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
 pub struct HostMetadata {
+    pub blocks: u64,
     pub ino: u64,
     pub kind: FileType,
+    pub nlink: u32,
+    pub rdev: u32,
+    pub atime: u64,
+    pub mtime: u64,
+    pub ctime: u64,
 }
 
 #[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
@@ -64,10 +104,14 @@ pub struct FsEntry {
 
 #[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub enum FsOpResponse {
+    Empty,
     GetAttr {
         entry: FsEntry,
     },
+    FileContent {
+        content: Vec<u8>,
+    },
     ReadDir {
         entries: Vec<FsEntry>,
-    }
+    },
 }
--- fortanix-vme/fortanix-vme-runner/src/fs.rs
+++ fortanix-vme/fortanix-vme-runner/src/fs.rs
@@ -20,14 +20,37 @@ impl VmeFs {
 
     pub fn handle_request(&self, request: FsOpRequest) -> IoResult<FsOpResponse> {
         match request {
-            FsOpRequest::GetAttr { ino } => {
-                let path = self.find_dir_by_ino(ino)?;
-                let entry = Self::read_fs_entry(&path)?;
+            FsOpRequest::Create { parent, name, metadata } => {
+                let entry = self.create(parent, name, metadata)?;
                 Ok(FsOpResponse::GetAttr { entry })
             }
+            FsOpRequest::GetAttr { ino } => {
+                let entry = self.getattr(ino)?;
+                Ok(FsOpResponse::GetAttr { entry })
+            }
+            FsOpRequest::Lookup { ino, name } => {
+                let entry = self.lookup(ino, name)?;
+                Ok(FsOpResponse::GetAttr { entry })
+            }
+            FsOpRequest::Mkdir { ino, name, metadata } => {
+                let entry = self.mkdir(ino, name, metadata)?;
+                Ok(FsOpResponse::GetAttr { entry })
+            }
+            FsOpRequest::Read { ino } => {
+                let content = self.read(ino)?;
+                Ok(FsOpResponse::FileContent { content })
+            }
             FsOpRequest::ReadDir { ino } => {
-                let entries = self.read_dir(ino)?;
+                let entries = self.readdir(ino)?;
                 Ok(FsOpResponse::ReadDir { entries })
+            }
+            FsOpRequest::SetAttr { ino, metadata } => {
+                let entry = self.setattr(ino, metadata)?;
+                Ok(FsOpResponse::GetAttr { entry })
+            }
+            FsOpRequest::Write { ino, content } => {
+                self.write(ino, content)?;
+                Ok(FsOpResponse::Empty)
             }
             _ => {
                 // Handle other requests (Create, Mkdir, Read, Write) as needed
@@ -36,10 +59,68 @@ impl VmeFs {
         }
     }
 
-    /// Iterates over files/directories and returns their metadata as a response.
-    fn read_dir(&self, ino: u64) -> IoResult<Vec<FsEntry>> {
+    fn create(&self, parent: u64, name: String, meta: Vec<u8>) -> IoResult<FsEntry> {
+        let parent_path = self.find_dir_by_ino(parent)?;
+        let path = parent_path.join(name);
+
+        match fs::File::create(&path) {
+            Ok(_) => {
+                if let Err(err) = Self::write_meta_file_for_path(&path, &meta) {
+                    let _ = fs::remove_file(path);
+                    return Err(err);
+                }
+
+                let entry = Self::read_fs_entry(&path)?;
+                Ok(entry)
+            }
+            Err(err) => return Err(err),
+        }
+    }
+
+    fn read(&self, ino: u64) -> IoResult<Vec<u8>> {
         let path = self.find_dir_by_ino(ino)?;
-        assert!(!Self::is_metadata_file(&path), "Metadata files should not be listed");
+        fs::read(path)
+    }
+
+    fn getattr(&self, ino: u64) -> IoResult<FsEntry> {
+        let path = self.find_dir_by_ino(ino)?;
+        let entry = Self::read_fs_entry(&path)?;
+        Ok(entry)
+    }
+
+    /// Locates a directory by given inode and returns `FsEntry` of
+    /// the file child to the located directory.
+    fn lookup(&self, ino: u64, name: String) -> IoResult<FsEntry> {
+        let parent_path = self.find_dir_by_ino(ino)?;
+        let path = parent_path.join(&name);
+        if !path.exists() {
+            return Err(Self::file_not_found_err(format!("File not found {} in requested dir (ino: {})", name, ino)));
+        }
+        let entry = Self::read_fs_entry(&path)?;
+        Ok(entry)
+    }
+
+    /// Creates a directory along with its meta file.
+    fn mkdir(&self, ino: u64, name: String, meta: Vec<u8>) -> IoResult<FsEntry> {
+        let parent_path = self.find_dir_by_ino(ino)?;
+        let path = parent_path.join(&name);
+        if path.exists() {
+            return Err(Self::already_exists_err(path.to_string_lossy().into()));
+        }
+        fs::create_dir(&path)?;
+        if let Err(err) = Self::write_meta_file_for_path(&path, &meta) {
+            let _ = fs::remove_dir(path);
+            return Err(err);
+        }
+
+        let entry = Self::read_fs_entry(&path)?;
+        Ok(entry)
+    }
+
+    /// Iterates over files/directories and returns their metadata as a response.
+    fn readdir(&self, ino: u64) -> IoResult<Vec<FsEntry>> {
+        let path = self.find_dir_by_ino(ino)?;
+        assert!(!Self::is_metadata_file(&path), "Metadata files should not be accessed directly.");
 
         let mut entries = Vec::new();
         let dir_entries = fs::read_dir(path)?;
@@ -57,6 +138,26 @@ impl VmeFs {
         Ok(entries)
     }
 
+    /// Fetches the related metafile associated with ino and updates metafile.
+    fn setattr(&self, ino: u64, metadata: Vec<u8>) -> IoResult<FsEntry> {
+        let path = self.find_dir_by_ino(ino)?;
+        assert!(!Self::is_metadata_file(&path), "Metadata files should not be accessed directly.");
+
+        // Ensure entry exists
+        let _ = Self::read_fs_entry(&path)?;
+        Self::write_meta_file_for_path(&path, &metadata)?;
+        let entry = Self::read_fs_entry(&path)?;
+        Ok(entry)
+    }
+
+    fn write(&self, ino: u64, content: Vec<u8>) -> IoResult<()> {
+        let path = self.find_dir_by_ino(ino)?;
+        assert!(!Self::is_metadata_file(&path), "Metadata files should not be accessed directly.");
+        fs::write(path, &content)?;
+
+        Ok(())
+    }
+
     fn is_metadata_file(path: &Path) -> bool {
         path.extension() == Some(OsStr::new("meta"))
     }
@@ -67,10 +168,7 @@ impl VmeFs {
         let mut meta_path = path.to_path_buf();
         meta_path.set_extension("meta");
         if !meta_path.exists() {
-            return Err(Error::new(
-                ErrorKind::NotFound,
-                format!("Metadata file not found for {:?}", path),
-            ));
+            return Err(Self::file_not_found_err(format!("Metadata file not found {:?}", path)));
         }
 
         let metadata = fs::read(meta_path)?;
@@ -87,7 +185,7 @@ impl VmeFs {
         }
 
         self.find_dir_by_ino_recursive(&self.root_path, ino)
-            .ok_or_else(|| Error::new(ErrorKind::NotFound, "Inode not found"))
+            .ok_or_else(|| Self::file_not_found_err("Inode not found".to_owned()))
     }
 
     fn find_dir_by_ino_recursive(&self, path: &Path, ino: u64) -> Option<PathBuf> {
@@ -112,5 +210,26 @@ impl VmeFs {
         }
 
         None
+    }
+
+    fn write_meta_file_for_path(path: &PathBuf, content: &[u8]) -> IoResult<()> {
+        let mut meta_path = path.clone();
+        meta_path.set_extension("meta");
+        fs::write(meta_path, content)?;
+        Ok(())
+    }
+
+    fn file_not_found_err(msg: String) -> Error {
+        Error::new(
+            ErrorKind::NotFound,
+            msg,
+        )
+    }
+
+    fn already_exists_err(msg: String) -> Error {
+        Error::new(
+            ErrorKind::AlreadyExists,
+            msg,
+        )
     }
 }
